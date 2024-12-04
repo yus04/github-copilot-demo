fff3641c3d935238110dd8ef3b233ac1278ebec3
yus04 <yuswim192@gmail.com>
2024-12-05 08:50:49 +0900

    add test.py

---
+++ github-copilot-chat/test.py
@@ -0,0 +1,13 @@
+# 2つの数字を加算する関数
+def add_numbers(a, b):
+    """
+    aとbを加算して返す関数
+    """
+    return a + b
+
+# 文字列を逆さにする関数
+def reverse_string(s):
+    """
+    文字列sを逆さにして返す関数
+    """
+    return s[::-1]
